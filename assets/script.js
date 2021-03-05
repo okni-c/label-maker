127ad1d8f39ad23cd346177d3bec129ff15bcaab
okni-c <oknistay@gmail.com>
2021-03-04 22:08:11 -0600

    added logic and toggle switch

--- assets/script.js
+++ assets/script.js
@@ -7,19 +7,68 @@
 //     }
 // })
 
-
+var body = document.body;
 
 var barcodeInput = document.getElementById("text1");
 
+var checkbox = document.querySelector("#auto-print");
+
+
 barcodeInput.addEventListener("keyup", function (event) {
     if (event.key === "Enter") {
+        var barcodeValue = document.getElementById("text1").value;
         event.preventDefault();
-        window.print();
+        console.log("BRUH");
+        var mainCode = document.createElement("h2");
+        mainCode.textContent = barcodeValue;
+        console.log(barcodeValue);
+        document.body.appendChild(mainCode);
+        mainCode.setAttribute('style', 'margin:auto; width:50%; text-align:center;');
+    }
+})
+
+checkbox.addEventListener('change', function () {
+    if (this.checked) {
+        barcodeInput.addEventListener("keyup", function (event) {
+            if (event.key === "Enter") {
+                var barcodeValue = document.getElementById("text1").value;
+                event.preventDefault();
+                console.log("BRUH");
+                var mainCode = document.createElement("h2");
+                mainCode.textContent = barcodeValue;
+                console.log(barcodeValue);
+                document.body.appendChild(mainCode);
+                mainCode.setAttribute('style', 'margin:auto; width:50%; text-align:center;');
+                window.print();
+            }
+        })
+    } else {
+        console.log("in else");
+        barcodeInput.addEventListener("keyup", function (event) {
+            if (event.key === "Enter") {
+                var barcodeValue = document.getElementById("text1").value;
+                event.preventDefault();
+                console.log("BRUH");
+                var mainCode = document.createElement("h2");
+                mainCode.textContent = barcodeValue;
+                console.log(barcodeValue);
+                document.body.appendChild(mainCode);
+                mainCode.setAttribute('style', 'margin:auto; width:50%; text-align:center;');
+            }
+        })
     }
 });
 
 
-function input() {
-    var barcodeValue = document.getElementById("text1").value;
-    alert("Text box got: " + barcodeValue);
-};
+
+
+
+// var checkbox = document.querySelector("input[name=checkbox]");
+
+// checkbox.addEventListener('change', function() {
+//   if (this.checked) {
+//     console.log("Checkbox is checked..");
+//   } else {
+//     console.log("Checkbox is not checked..");
+//   }
+// });
